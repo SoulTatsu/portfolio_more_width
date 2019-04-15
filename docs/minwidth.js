ec972814f13f2b7a247ef941ce7fa27a832d65e8
Michał Liczbik <ultralesbik@gmail.com>
2019-04-15 14:01:52 +0200

    new

---
+++ docs/minwidth.js
@@ -0,0 +1,5 @@
+if (screen.width > 1000) {
+    window.location.replace("https://soultatsu.github.io/portfolio/bigScreen.html");
+} else {
+    window.location.replace("https://soultatsu.github.io/portfolio/");
+}
